7ce86bb651cbcaac5c787048b45de78cf4064496
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2025-09-05 14:55:12 -0300

    Inicio_Projeto:IMC

---
+++ 04_IMC_Calculator/dados.py
@@ -0,0 +1,6 @@
+import ttkbootstrap as ttk
+
+class Janela_dados:
+    def __init__(self):
+        self.janela = ttk.Window(title="Ms.Gothart Alternativa", themename= "vapor")
+        self.janela.geometry("1200x800")
